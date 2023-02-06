0e144b5f0ea77d3600201ebe5a95ab4a6d595ed4
tuansdf <103062682+tuansdf@users.noreply.github.com>
2023-02-06 13:53:35 +0700

    feat: set up color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "hsl(172, 67%, 45%)",
+      neutral: {
+        0: "hsl(0, 0%, 100%)",
+        100: "hsl(189, 41%, 97%)",
+        200: "hsl(185, 41%, 84%)",
+        300: "hsl(184, 14%, 56%)",
+        400: "hsl(186, 14%, 43%)",
+        500: "hsl(183, 100%, 15%)",
+      },
+    },
   },
   plugins: [],
 };
